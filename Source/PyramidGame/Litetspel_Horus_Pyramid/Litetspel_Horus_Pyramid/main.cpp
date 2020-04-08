f0d830567fce1d722dc0a546c43df757d3922d63
EdvinWendt <EdvinWendt@users.noreply.github.com>
2020-04-08 11:06:23 +0200

    Test

--- Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/main.cpp
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/main.cpp
@@ -193,7 +193,7 @@ void initDeviceAndSwapChain()
 
 	UINT flags = D3D11_CREATE_DEVICE_SINGLETHREADED;
 	#if defined( DEBUG ) || defined( _DEBUG )
-		flags |= D3D11_CREATE_DEVICE_DEBUG;
+		//flags |= D3D11_CREATE_DEVICE_DEBUG;
 	#endif
 
 	HRESULT hr = D3D11CreateDeviceAndSwapChain(
