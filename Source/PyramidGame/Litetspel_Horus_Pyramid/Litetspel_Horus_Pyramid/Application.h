96e7a5ddef948b1879164c1bdeb1e580162d9bf8
Oskar Hansen <oskar_ozkizz_wow@hotmail.com>
2020-04-08 13:17:56 +0200

    Base application class

---
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/Application.h
@@ -0,0 +1,52 @@
+#ifndef _APPLICATION_H_
+#define _APPLICATION_H_
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+// Standard
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <comdef.h>
+#include <assert.h>
+#include <memory>
+#include <wrl/client.h>
+
+struct GameOptions
+{
+	int width;
+	int height;
+
+};
+
+class Application
+{
+public:
+	static Application& getInstance()
+	{
+		static Application instance;
+
+		return instance;
+
+	}
+private:
+
+	Application();
+
+
+
+public:
+	
+
+	Application(Application const&) = delete;
+	void operator=(Application const&) = delete;
+	bool initApplication(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd, int screenWidth, int screenHeight, int showCmd);
+	void createWin32Window(HINSTANCE hInstance, const wchar_t* windowTitle, HWND& _d3d11Window);
+	HWND m_window;
+	GameOptions m_gameOptions;
+
+};
+
+
+#endif // !_APPLICATION_H_
