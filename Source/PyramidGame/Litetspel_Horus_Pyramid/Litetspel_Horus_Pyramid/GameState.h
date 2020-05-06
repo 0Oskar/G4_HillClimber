60aef74e4e43648639e1d3ae6dd4bd7e5a50692d
Oskar Hansen <oskar_ozkizz_wow@hotmail.com>
2020-05-06 12:55:31 +0200

    On portal entry change active room and send new room data

--- Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.h
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.h
@@ -39,7 +39,7 @@ public:
 	std::vector<ConstBuffer<VS_CONSTANT_BUFFER>>* getWvpCBuffersPtr();
 	void addGameObjectToWorld(bool dynamic, bool colide, float weight, int mdlIndx, Model* mdl, DirectX::XMVECTOR position, DirectX::XMVECTOR scale3D, DirectX::XMFLOAT3 boundingBoxSize, DirectX::XMFLOAT3 acceleration, DirectX::XMFLOAT3 deceleration);
 	void addPlatformToWorld(int mdlIndex, DirectX::BoundingOrientedBox* pyramid, Model* mdl, DirectX::XMVECTOR position, DirectX::XMFLOAT3 platformBoundingBox);
-	void addPortalToWorld(XMVECTOR teleportLocation, int mdlIndx, Model* mdl, DirectX::XMVECTOR position, DirectX::XMVECTOR scale3D, DirectX::XMFLOAT3 boundingBoxSize);
+	void addPortalToWorld(XMVECTOR teleportLocation, int mdlIndx, Model* mdl, DirectX::XMVECTOR position, DirectX::XMVECTOR scale3D, DirectX::XMFLOAT3 boundingBoxSize, Room* room = nullptr);
 
 	// Initialization
 	void initlialize(ID3D11Device* device, ID3D11DeviceContext* dContext, GameOptions options, std::shared_ptr<DirectX::AudioEngine> audioEngine);
--- Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/Portal.h
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/Portal.h
@@ -1,6 +1,7 @@
 #pragma once
 #include"GameObject.h"
 #include "Player.h"
+#include "Room.h"
 
 class Portal : public GameObject
 {
@@ -8,12 +9,16 @@ private:
 
 	XMVECTOR m_teleportLocation;
 	Player* player;
-
+	Room* m_roomPtr;
+	bool m_changeActiveRoom;
 public:
 
 	Portal();
 	~Portal();
-	void initialize(int modelIndex, int wvpCBufferIndex, Model* mdl, XMVECTOR teleportLocation, Player* player);
+	void initialize(int modelIndex, int wvpCBufferIndex, Model* mdl, XMVECTOR teleportLocation, Player* player, Room* room = nullptr);
 	void update();
+	void resetActiveRoomVariable();
+	bool shouldChangeActiveRoom();
+	Room* getRoomPtr();
 
 };
