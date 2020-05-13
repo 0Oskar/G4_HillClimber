2de619032449da95a3701455531d6a203397ab43
Medo Osman <medoosman@outlook.com>
2020-05-13 10:32:18 +0200

    Fixed checkpoint hook collision

--- Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.cpp
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.cpp
@@ -366,6 +366,19 @@ void GameState::initlialize(ID3D11Device* device, ID3D11DeviceContext* dContext,
 		}
 	}
 
+	// Checkpoints
+	vec = DirectX::XMVectorSet(10.f, 85.f, 114.f, 1.f);
+	this->addGameObjectToWorld(false, true, 0, 16, &m_models[16], vec, { 0.7f, 0.7f, 0.7f }, DirectX::XMFLOAT3(7.f, 1.f, 5.f));
+	this->m_gameObjects.back()->setRotation({ 0.f, 1.57f, 0.f });
+	this->m_checkpointHandler.addCheckpointGameObject((int)this->m_gameObjects.size() - 1, vec);
+	platformBB.emplace_back(this->m_gameObjects.back()->getAABBPtr());
+
+	vec = DirectX::XMVectorSet(0.f, 25.f, 42.f, 1.f);
+	this->addGameObjectToWorld(false, true, 0, 16, &m_models[16], vec, { 0.7f, 0.7f, 0.7f }, DirectX::XMFLOAT3(7.f, 1.f, 5.f));
+	this->m_gameObjects.back()->setRotation({ 0.f, 1.57f, 0.f });
+	this->m_checkpointHandler.addCheckpointGameObject((int)this->m_gameObjects.size() - 1, vec);
+	platformBB.emplace_back(this->m_gameObjects.back()->getAABBPtr());
+
 	this->m_player.updateHookHandBB(platformBB);
 
 	//Initialize audio component for platforms and add theire boundingboxes to playerBoundingBoxes
@@ -378,17 +391,6 @@ void GameState::initlialize(ID3D11Device* device, ID3D11DeviceContext* dContext,
 			castToPlatform->initAudioComponent(audioEngine, m_player.getMoveCompPtr());
 		}
 	}
-
-	// Checkpoints
-	vec = DirectX::XMVectorSet(10.f, 85.f, 114.f, 1.f);
-	this->addGameObjectToWorld(false, true, 0, 16, &m_models[16], vec, {0.7f, 0.7f, 0.7f}, DirectX::XMFLOAT3(7.f, 1.f, 5.f));
-	this->m_gameObjects.back()->setRotation({0.f, 1.57f, 0.f});
-	this->m_checkpointHandler.addCheckpointGameObject((int)this->m_gameObjects.size() - 1, vec);
-
-	vec = DirectX::XMVectorSet(0.f, 25.f, 42.f, 1.f);
-	this->addGameObjectToWorld(false, true, 0, 16, &m_models[16], vec, { 0.7f, 0.7f, 0.7f }, DirectX::XMFLOAT3(7.f, 1.f, 5.f));
-	this->m_gameObjects.back()->setRotation({ 0.f, 1.57f, 0.f });
-	this->m_checkpointHandler.addCheckpointGameObject((int)this->m_gameObjects.size() - 1, vec);
 
 	// Camera
 	this->m_camera.followMoveComp(this->m_player.getMoveCompPtr());
