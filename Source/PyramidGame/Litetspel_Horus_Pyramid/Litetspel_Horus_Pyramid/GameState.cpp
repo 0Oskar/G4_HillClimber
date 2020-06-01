1c86272b278c1317ae3329834119d8d74d205107
Viktor Bodeang <62887027+vibbo99@users.noreply.github.com>
2020-06-01 11:24:30 +0200

    Fast test

--- Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.cpp
+++ Source/PyramidGame/Litetspel_Horus_Pyramid/Litetspel_Horus_Pyramid/GameState.cpp
@@ -567,12 +567,12 @@ void GameState::initlialize(ID3D11Device* device, ID3D11DeviceContext* dContext,
 	this->m_rooms.emplace_back(new PyramidRoom());
 	this->m_rooms.back()->initialize(m_device, m_dContext, m_modelsPtr, &this->m_wvpCBuffers, &m_player, XMVectorSet(0, 0, 0, 1), audioEngine, &this->m_gameTime, options);
 	dynamic_cast<PyramidRoom*>(this->m_rooms.back())->init(&m_pyramidOBB);
-	m_activeRoom = m_rooms.back();
 
 	//Template Room [1] Viktor
 	this->m_rooms.emplace_back(new FindGemsRoom());
 	this->m_rooms.back()->initialize(m_device, m_dContext, m_modelsPtr, &this->m_wvpCBuffers, &m_player, XMVectorSet(0, 0, -300, 1), audioEngine, &this->m_gameTime, options);
 	dynamic_cast<FindGemsRoom*>(this->m_rooms.back())->init();
+	m_activeRoom = m_rooms.back();
 
 	//Kevin Room [2]
 	this->m_rooms.emplace_back(new KevinsRoom());
